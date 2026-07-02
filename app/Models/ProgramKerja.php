25a1b38ef9424b5533d6a5f1f071544a28af2f1d
SatyatmaRaka <satyatma.raka@gmail.com>
2026-07-02 10:45:11 +0700

    Add migration & model: anggota, program_kerja, kegiatan, galeri, pengaturan (singular table names)

---
+++ app/Models/ProgramKerja.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProgramKerja extends Model
+{
+    use HasFactory;
+
+    protected $table = 'program_kerja';
+
+    protected $fillable = ['judul', 'tema', 'deskripsi', 'icon', 'pic', 'status', 'urutan'];
+}
